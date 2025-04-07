e7a38fe3e38ad31ca5a3014f1e73d80ef49a8069
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-04-07 11:58:36 +0800

    ask user to input quiz questions until the user quits

--- quiz_creator.py
+++ quiz_creator.py
@@ -6,3 +6,16 @@
 #  d
 # answer
 # save into txt file
+
+print('Quiz Creator')
+#ask the user for the question, choices, and answer
+while True:
+    question = input('Input your question (type "exit" to stop): ')
+    if question.lower() == "exit":
+        break
+    print('Input your choices')
+    choice_a = input('Input choice a: ')
+    choice_b = input('Input choice b: ')
+    choice_c = input('Input choice c: ')
+    choice_d = input('Input choice d: ')
+    answer = input('Input the correct answer from the choices: ')
